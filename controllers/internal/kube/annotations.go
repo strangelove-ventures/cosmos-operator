9b1d0135de9a21ae634455c03c7cb83d21e5b695
David Nix <hello@davidnix.io>
2022-08-12 09:32:08 -0600

    Add warning

--- controllers/internal/kube/annotations.go
+++ controllers/internal/kube/annotations.go
@@ -2,5 +2,6 @@ package kube
 
 const (
 	// OrdinalAnnotation denotes the resource's ordinal position.
+	// This annotation key must never be changed, or it will be a backward breaking change for operator upgrades.
 	OrdinalAnnotation = "app.kubernetes.io/ordinal"
 )
