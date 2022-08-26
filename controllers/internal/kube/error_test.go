dda1521c0fdb1e40aaf4aeb3618d707b87bd0789
David Nix <DavidNix@users.noreply.github.com>
2022-08-25 19:33:30 -0600

    feat: Option to enable profiling CPU and memory (#71)

--- controllers/internal/kube/error_test.go
+++ controllers/internal/kube/error_test.go
@@ -15,10 +15,10 @@ func TestReconcileError(t *testing.T) {
 	terr := TransientError(err)
 	require.True(t, terr.IsTransient())
 	require.ErrorIs(t, terr, err)
-	require.EqualError(t, terr, "transient error: boom")
+	require.EqualError(t, terr, "boom")
 
 	rerr := UnrecoverableError(err)
 	require.False(t, rerr.IsTransient())
 	require.ErrorIs(t, rerr, err)
-	require.EqualError(t, rerr, "unrecoverable error: boom")
+	require.EqualError(t, rerr, "boom")
 }
