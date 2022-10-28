a2fecced809500f23c94d1dd438f284ce3bafc42
David Nix <hello@davidnix.io>
2022-10-28 09:14:28 -0600

    Remove duplicate import

--- controllers/hostedsnapshot_controller.go
+++ controllers/hostedsnapshot_controller.go
@@ -27,8 +27,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
 )
 
 // HostedSnapshotReconciler reconciles a HostedSnapshot object
@@ -43,10 +41,6 @@ type HostedSnapshotReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the HostedSnapshot object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
