51065ceeb5dfa6f698604af6dc135200cdc6b4f0
David Nix <DavidNix@users.noreply.github.com>
2022-11-18 10:58:45 -0700

    refactor(StatefulJob): Remove prefix from resource names (#148)

--- controllers/internal/statefuljob/resource_test.go
+++ controllers/internal/statefuljob/resource_test.go
@@ -9,13 +9,14 @@ import (
 )
 
 func TestResourceName(t *testing.T) {
+	t.Parallel()
+
 	var crd cosmosalpha.StatefulJob
 	crd.Name = "test"
 
-	require.Equal(t, "stateful-job-test", ResourceName(&crd))
+	require.Equal(t, "test", ResourceName(&crd))
 
 	crd.Name = strings.Repeat("long", 100)
 	name := ResourceName(&crd)
 	require.LessOrEqual(t, 253, len(name))
-	require.Contains(t, name, "stateful-job-")
 }
