96e500ba866008ce40c0d29da602ddc800232e2b
David Nix <hello@davidnix.io>
2022-12-05 11:41:04 -0700

    Implement reset status

---
+++ controllers/internal/volsnapshot/status.go
@@ -0,0 +1,17 @@
+package volsnapshot
+
+import (
+	"time"
+
+	cosmosalpha "github.com/strangelove-ventures/cosmos-operator/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ResetStatus resets the CRD's status to appropriate values for the start of a reconcile loop.
+func ResetStatus(crd *cosmosalpha.ScheduledVolumeSnapshot) {
+	crd.Status.ObservedGeneration = crd.Generation
+	crd.Status.StatusMessage = nil
+	if crd.Status.CreatedAt.IsZero() {
+		crd.Status.CreatedAt = metav1.NewTime(time.Now())
+	}
+}
