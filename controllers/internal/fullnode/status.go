b3dca37c8ac7dca8ee06965e09590c2f9d557634
David Nix <hello@davidnix.io>
2022-12-05 10:34:10 -0700

    Remove volume snapshot from fullnode status

--- controllers/internal/fullnode/status.go
+++ controllers/internal/fullnode/status.go
@@ -1,10 +1,7 @@
 package fullnode
 
 import (
-	"time"
-
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // ResetStatus is used at the beginning of the reconcile loop.
@@ -13,17 +10,4 @@ func ResetStatus(crd *cosmosv1.CosmosFullNode) {
 	crd.Status.ObservedGeneration = crd.Generation
 	crd.Status.Phase = cosmosv1.FullNodePhaseProgressing
 	crd.Status.StatusMessage = nil
-
-	if crd.Spec.VolumeSnapshot == nil {
-		crd.Status.VolumeSnapshot = nil
-		return
-	}
-
-	if crd.Status.VolumeSnapshot == nil {
-		crd.Status.VolumeSnapshot = new(cosmosv1.VolumeSnapshotStatus)
-	}
-
-	if crd.Status.VolumeSnapshot.ActivatedAt.IsZero() {
-		crd.Status.VolumeSnapshot.ActivatedAt = metav1.NewTime(time.Now())
-	}
 }
