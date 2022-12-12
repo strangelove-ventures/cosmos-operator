07ba483b910740b9e80450c62b1dbd99ac8d1a05
David Nix <hello@davidnix.io>
2022-12-12 15:38:37 -0700

    And deleting snapshots

--- controllers/scheduledvolumesnapshot_controller.go
+++ controllers/scheduledvolumesnapshot_controller.go
@@ -58,7 +58,7 @@ func NewScheduledVolumeSnapshotReconciler(
 //+kubebuilder:rbac:groups=cosmos.strange.love,resources=scheduledvolumesnapshots/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cosmos.strange.love,resources=scheduledvolumesnapshots/finalizers,verbs=update
 //+kubebuilder:rbac:groups=cosmos.strange.love,resources=cosmosfullnodes/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=snapshot.storage.k8s.io,resources=volumesnapshots,verbs=get;create
+//+kubebuilder:rbac:groups=snapshot.storage.k8s.io,resources=volumesnapshots,verbs=get;create;delete
 //+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
