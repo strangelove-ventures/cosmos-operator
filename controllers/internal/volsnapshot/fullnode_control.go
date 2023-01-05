19ef61bfe0e0062f62c19224b2d27302e45969bd
David Nix <hello@davidnix.io>
2023-01-05 12:39:28 -0700

    Implement confirming pod deletion

--- controllers/internal/volsnapshot/fullnode_control.go
+++ controllers/internal/volsnapshot/fullnode_control.go
@@ -7,22 +7,30 @@ import (
 
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
 	cosmosalpha "github.com/strangelove-ventures/cosmos-operator/api/v1alpha1"
+	"github.com/strangelove-ventures/cosmos-operator/controllers/internal/kube"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// Patcher patches the status subresource of a CosmosFullNode.
-type Patcher interface {
+// StatusPatcher patches the status subresource of a CosmosFullNode.
+type StatusPatcher interface {
 	Patch(ctx context.Context, obj client.Object, patch client.Patch, opts ...client.PatchOption) error
+}
+
+// Lister lists resources.
+type Lister interface {
+	List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error
 }
 
 // FullNodeControl manages a ScheduledVolumeSnapshot's spec.fullNodeRef.
 type FullNodeControl struct {
-	statusClient Patcher
+	listClient   Lister
+	statusClient StatusPatcher
 }
 
-func NewFullNodeControl(statusClient Patcher) *FullNodeControl {
-	return &FullNodeControl{statusClient: statusClient}
+func NewFullNodeControl(statusClient StatusPatcher, listClient Lister) *FullNodeControl {
+	return &FullNodeControl{listClient: listClient, statusClient: statusClient}
 }
 
 // SignalPodDeletion updates the FullNodeRef's status to indicate it should delete the pod candidate.
@@ -57,4 +65,23 @@ func (control FullNodeControl) sourceKey(crd *cosmosalpha.ScheduledVolumeSnapsho
 	key := strings.Join([]string{crd.Namespace, crd.Name, cosmosalpha.GroupVersion.Version, cosmosalpha.GroupVersion.Group}, ".")
 	// Remove all slashes because key is used in JSONPatch where slash "/" is a reserved character.
 	return strings.ReplaceAll(key, "/", "")
+}
+
+// ConfirmPodDeletion returns a nil error if the pod is deleted.
+// Any non-nil error is transient, including if the pod has not been deleted yet.
+// Assumes crd's status.candidate is set, otherwise this method panics.
+func (control FullNodeControl) ConfirmPodDeletion(ctx context.Context, crd *cosmosalpha.ScheduledVolumeSnapshot) error {
+	var pods corev1.PodList
+	if err := control.listClient.List(ctx, &pods,
+		client.InNamespace(crd.Spec.FullNodeRef.Namespace),
+		client.MatchingFields{kube.ControllerOwnerField: crd.Spec.FullNodeRef.Name},
+	); err != nil {
+		return fmt.Errorf("list pods: %w", err)
+	}
+	for _, pod := range pods.Items {
+		if pod.Name == crd.Status.Candidate.PodName {
+			return fmt.Errorf("pod %s not deleted yet", pod.Name)
+		}
+	}
+	return nil
 }
