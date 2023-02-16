294c516ae0b073a0e8fd423267fefff26681a87d
David Nix <hello@davidnix.io>
2023-02-16 16:35:56 -0700

    More api design tweaking

--- api/v1/self_healing_types.go
+++ api/v1/self_healing_types.go
@@ -5,26 +5,32 @@ import "k8s.io/apimachinery/pkg/util/intstr"
 // SelfHealingSpec is part of a CosmosFullNode but is managed by a separate controller, SelfHealingController.
 // This is an effort to reduce complexity in the CosmosFullNodeController.
 type SelfHealingSpec struct {
-	// Determines when to destroy and recreate a replica (aka instance, pod/pvc combo) that is failing.
+	// Determines when to destroy and recreate a replica (aka pod/pvc combo) that is crashlooping.
 	// Occasionally, data may become corrupt and the chain exits and cannot restart.
-	// This strategy only watches the pods "node" container running the `start` command, and only for
-	// pods that are crashlooping.
+	// This strategy only watches the pods' "node" containers running the `start` command.
 	//
 	// This pairs well with volumeClaimTemplate.autoDataSource and a ScheduledVolumeSnapshot resource.
 	// With this pairing, a new PVC is created with a recent VolumeSnapshot.
 	// Otherwise, ensure your snapshot, genesis, etc. creation are idempotent.
 	// (e.g. chain.snapshotURL and chain.genesisURL have stable urls)
+	//
+	// This feature may be extended to detect other failed pod states instead of just crashloops.
 	// +optional
-	ReplicaFaultRecovery *ReplicaFaultRecoverySpec `json:"replicaFaultRecovery"`
+	PodFaultRecovery *PodFaultRecovery `json:"podFaultRecovery"`
 }
 
-type ReplicaFaultRecoverySpec struct {
+type PodFaultRecovery struct {
 	// How many healthy pods are required to trigger destroying a crashlooping pod and pvc.
+	// Set an integer or a percentage string such as 50%.
+	//
+	// This setting attempts to minimize false positives in order to detect data corruption instead of
+	// a variety of other reasons for crashloops.
 	// The controller periodically inspects the status of all pods.
 	// If the majority of pods are crashlooping, then there's probably something else wrong, and recreating
 	// the pod and pvc will have no effect.
-	// Set an integer or a percentage string such as 50%.
-	// If the threshold is too high, defaults to watching for 1 unhealthy pod.
+	//
+	// If the threshold is too high, defaults to recovering 1 unhealthy pod, the rest must be healthy.
+	// It's not recommended to use this feature with only 1 replica.
 	HealthyThreshold intstr.IntOrString `json:"healthyThreshold"`
 
 	// How many restarts to wait before destroying and recreating an unhealthy replica.
