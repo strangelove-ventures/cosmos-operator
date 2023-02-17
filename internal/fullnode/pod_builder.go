4cdaaf973a62db4575c9de8a4c3fc8d65f51905a
David Nix <hello@davidnix.io>
2023-02-17 14:35:26 -0700

    wip: Test resource requests

--- internal/fullnode/pod_builder.go
+++ internal/fullnode/pod_builder.go
@@ -230,6 +230,7 @@ func (b PodBuilder) WithOrdinal(ordinal int32) PodBuilder {
 			{Name: volConfig, MountPath: tmpConfigDir},
 		}...)
 	}
+
 	for i := range pod.Spec.Containers {
 		pod.Spec.Containers[i].VolumeMounts = mounts
 	}
@@ -335,8 +336,18 @@ config-merge -f toml "$TMP_DIR/app.toml" "$OVERLAY_DIR/app-overlay.toml" > "$CON
 	}
 
 	// Setting init container resources helps GKE AutoPilot provision node resources more accurately.
+	resources := corev1.ResourceRequirements{
+		Requests: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceCPU:    resource.MustParse("500m"),
+			corev1.ResourceMemory: resource.MustParse("1Gi"),
+		},
+		//Limits: map[corev1.ResourceName]resource.Quantity{
+		//	corev1.ResourceCPU:    resource.MustParse("1000m"),
+		//	corev1.ResourceMemory: resource.MustParse("1Gi"),
+		//},
+	}
 	for i := range containers {
-		containers[i].Resources = tpl.Resources
+		containers[i].Resources = resources
 	}
 
 	return containers
