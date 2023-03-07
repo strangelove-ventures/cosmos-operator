b753b520cef6e642af0d2aed9d5ba0af95d9dbc9
David Nix <hello@davidnix.io>
2023-03-07 14:46:10 -0700

    Refactor healthcheck client to take host

--- internal/healthcheck/client.go
+++ internal/healthcheck/client.go
@@ -5,26 +5,35 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 // Client can be used to query healthcheck information.
 type Client struct {
-	rootURL string
-	httpDo  func(req *http.Request) (*http.Response, error)
+	httpDo func(req *http.Request) (*http.Response, error)
 }
 
 func NewClient(client *http.Client) *Client {
 	return &Client{
-		rootURL: fmt.Sprintf("http://localhost:%d", Port),
-		httpDo:  client.Do,
+		httpDo: client.Do,
 	}
 }
 
 // DiskUsage returns disk usage statistics or an error if unable to obtain.
-func (c Client) DiskUsage(ctx context.Context) (DiskUsageResponse, error) {
+// Do not include the port in the host.
+func (c Client) DiskUsage(ctx context.Context, host string) (DiskUsageResponse, error) {
 	var diskResp DiskUsageResponse
-	req, err := http.NewRequestWithContext(ctx, "GET", c.rootURL+"/disk", nil)
+	u, err := url.Parse(host)
+	if err != nil {
+		return diskResp, fmt.Errorf("url parse: %w", err)
+	}
+	u.Host = net.JoinHostPort(u.Host, strconv.Itoa(Port))
+	u.Path = "/disk"
+
+	req, err := http.NewRequestWithContext(ctx, "GET", u.String(), nil)
 	if err != nil {
 		return diskResp, fmt.Errorf("new request: %w", err)
 	}
