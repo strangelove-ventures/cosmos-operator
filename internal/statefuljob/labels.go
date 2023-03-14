9fae929dc92997e4dde7a0f1bfbb1e4f21d2bc44
David Nix <hello@davidnix.io>
2023-03-14 12:05:23 -0600

    Replace other constants

--- internal/statefuljob/labels.go
+++ internal/statefuljob/labels.go
@@ -1,12 +1,13 @@
 package statefuljob
 
 import (
+	cosmosalpha "github.com/strangelove-ventures/cosmos-operator/api/v1alpha1"
 	"github.com/strangelove-ventures/cosmos-operator/internal/kube"
 )
 
 func defaultLabels() map[string]string {
 	return map[string]string{
 		kube.ControllerLabel: "cosmos-operator",
-		kube.ComponentLabel:  "StatefulJob",
+		kube.ComponentLabel:  cosmosalpha.StatefulJobController,
 	}
 }
