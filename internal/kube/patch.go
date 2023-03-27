7d4ade3f3a48b7a88fccf1ce2decb40ac8b8aaae
David Nix <hello@davidnix.io>
2023-03-27 17:35:39 -0600

    Finalize strategic patch method

--- internal/kube/patch.go
+++ internal/kube/patch.go
@@ -1,72 +1,30 @@
 package kube
 
 import (
-	"fmt"
-
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
 
 var converter = runtime.DefaultUnstructuredConverter
 
-// ApplyStrategicPatch computes a strategic patch from original and patch, and applies the patch to the original.
+// ApplyStrategicPatch applies a strategic merge patch to a target object.
 // Inspired by: https://github.com/kubernetes/apiserver/blob/45f55ded302a02ed2023e8b45bd241cf7d81169e/pkg/endpoints/handlers/patch.go
-func ApplyStrategicPatch[T any](original, patch T) error {
-	//origJSON, err := json.Marshal(original)
-	//if err != nil {
-	//	return err
-	//}
-	//patchJSON, err := json.Marshal(patch)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//schema, err := strategicpatch.NewPatchMetaFromStruct(original)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//applyPatch, err := strategicpatch.CreateTwoWayMergePatchUsingLookupPatchMeta(origJSON, patchJSON, schema)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//fmt.Println("Patch:", string(applyPatch))
-	//
-	//result, err := strategicpatch.StrategicMergePatchUsingLookupPatchMeta(origJSON, applyPatch, schema)
-	//if err != nil {
-	//	return err
-	//}
-	//fmt.Println("Orig:", string(origJSON))
-	//fmt.Println("Result:", string(result))
-
-	origMap, err := converter.ToUnstructured(original)
+func ApplyStrategicPatch[T any](target, patch T) error {
+	origMap, err := converter.ToUnstructured(target)
 	if err != nil {
-		panic(err)
 		return err
 	}
 	patchMap, err := converter.ToUnstructured(patch)
 	if err != nil {
-		panic(err)
 		return err
 	}
-
-	schema, err := strategicpatch.NewPatchMetaFromStruct(original)
+	schema, err := strategicpatch.NewPatchMetaFromStruct(target)
 	if err != nil {
-		panic(err)
 		return err
 	}
-
 	result, err := strategicpatch.StrategicMergeMapPatchUsingLookupPatchMeta(origMap, patchMap, schema)
-	fmt.Println("Result:", result)
 	if err != nil {
-		panic(err)
 		return err
 	}
-
-	fmt.Println("Original before:", original)
-	err = runtime.DefaultUnstructuredConverter.FromUnstructured(result, original)
-	fmt.Println("Original after:", original)
-	return err // TODO: useless return
-	return nil
+	return runtime.DefaultUnstructuredConverter.FromUnstructured(result, target)
 }
--- internal/kube/patch_test.go
+++ internal/kube/patch_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestStrategicPatch(t *testing.T) {
 	t.Run("happy path", func(t *testing.T) {
-		origObj := &corev1.PodTemplateSpec{
+		target := &corev1.PodTemplateSpec{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "test",
 				Labels: map[string]string{
@@ -34,34 +34,24 @@ func TestStrategicPatch(t *testing.T) {
 			},
 		}
 
-		patch := origObj.DeepCopy()
-		patch.Labels["app"] = "CHANGED"
-		patch.Spec.RestartPolicy = corev1.RestartPolicyNever
-		patch.Spec.Containers = []corev1.Container{
-			{
-				Name:  "app",
-				Image: "myapp:CHANGED",
+		patch := &corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{
+					"app": "CHANGED",
+				},
+			},
+			Spec: corev1.PodSpec{
+				RestartPolicy: corev1.RestartPolicyNever,
+				Containers: []corev1.Container{
+					{
+						Name:  "app",
+						Image: "myapp:CHANGED",
+					},
+				},
 			},
 		}
 
-		//patch := &corev1.PodTemplateSpec{
-		//	ObjectMeta: metav1.ObjectMeta{
-		//		Labels: map[string]string{
-		//			"app": "CHANGED",
-		//		},
-		//	},
-		//	Spec: corev1.PodSpec{
-		//		RestartPolicy: corev1.RestartPolicyNever,
-		//		Containers: []corev1.Container{
-		//			{
-		//				Name:  "app",
-		//				Image: "myapp:CHANGED",
-		//			},
-		//		},
-		//	},
-		//}
-
-		err := ApplyStrategicPatch(origObj, patch)
+		err := ApplyStrategicPatch(target, patch)
 
 		require.NoError(t, err)
 
@@ -88,10 +78,37 @@ func TestStrategicPatch(t *testing.T) {
 				},
 			},
 		}
-		require.Equal(t, want, origObj)
+		require.Equal(t, want, target)
 	})
 
 	t.Run("identity", func(t *testing.T) {
+		obj := &corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+				Labels: map[string]string{
+					"foo": "bar",
+				},
+			},
+			Spec: corev1.PodSpec{
+				NodeSelector:  map[string]string{"test": "value"},
+				RestartPolicy: corev1.RestartPolicyAlways,
+				Containers: []corev1.Container{
+					{
+						Name:  "app",
+						Image: "myapp:v1",
+					},
+					{
+						Name:  "second",
+						Image: "v2",
+					},
+				},
+			},
+		}
 
+		want := obj.DeepCopy()
+
+		err := ApplyStrategicPatch(obj, obj)
+		require.NoError(t, err)
+		require.Equal(t, want, obj)
 	})
 }
