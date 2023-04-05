2b539aa5de6264e9dfe266a263065991d214edcc
David Nix <hello@davidnix.io>
2023-04-05 10:22:37 -0600

    Use full path to file again

--- internal/fullnode/pod_builder.go
+++ internal/fullnode/pod_builder.go
@@ -252,7 +252,7 @@ func (b PodBuilder) WithOrdinal(ordinal int32) PodBuilder {
 
 	// At this point, guaranteed to have at least one container.
 	pod.Spec.Containers[0].VolumeMounts = append(mounts, corev1.VolumeMount{
-		Name: volNodeKey, MountPath: path.Join(ChainHomeDir, "config"), SubPath: nodeKeyFile,
+		Name: volNodeKey, MountPath: path.Join(ChainHomeDir, "config", nodeKeyFile), SubPath: nodeKeyFile,
 	})
 
 	b.pod = pod
