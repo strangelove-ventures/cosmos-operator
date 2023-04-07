a1441785189a97b592b5249cb59ba16301cc3dd6
David Nix <DavidNix@users.noreply.github.com>
2023-04-07 14:03:08 -0600

    refactor(CosmosFullNode): PVCs use new diffing (#265)

--- internal/fullnode/pvc_builder.go
+++ internal/fullnode/pvc_builder.go
@@ -1,14 +1,11 @@
 package fullnode
 
 import (
-	"encoding/hex"
 	"fmt"
-	"hash/fnv"
-	"sort"
 
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
+	"github.com/strangelove-ventures/cosmos-operator/internal/diff"
 	"github.com/strangelove-ventures/cosmos-operator/internal/kube"
-	"golang.org/x/exp/maps"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -18,22 +15,20 @@ var (
 )
 
 // BuildPVCs outputs desired PVCs given the crd.
-func BuildPVCs(crd *cosmosv1.CosmosFullNode) []*corev1.PersistentVolumeClaim {
+func BuildPVCs(crd *cosmosv1.CosmosFullNode) []diff.Resource[*corev1.PersistentVolumeClaim] {
 	base := corev1.PersistentVolumeClaim{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
 			Kind:       "PersistentVolumeClaim",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace: crd.Namespace,
-			Labels: defaultLabels(crd,
-				kube.RevisionLabel, pvcRevisionHash(crd),
-			),
+			Namespace:   crd.Namespace,
+			Labels:      defaultLabels(crd),
 			Annotations: make(map[string]string),
 		},
 	}
 
-	var pvcs []*corev1.PersistentVolumeClaim
+	var pvcs []diff.Resource[*corev1.PersistentVolumeClaim]
 	for i := int32(0); i < crd.Spec.Replicas; i++ {
 		if pvcDisabled(crd, i) {
 			continue
@@ -43,7 +38,6 @@ func BuildPVCs(crd *cosmosv1.CosmosFullNode) []*corev1.PersistentVolumeClaim {
 		name := pvcName(crd, i)
 		pvc.Name = name
 		pvc.Labels[kube.InstanceLabel] = instanceName(crd, i)
-		pvc.Annotations[kube.OrdinalAnnotation] = kube.ToIntegerValue(i)
 
 		tpl := crd.Spec.VolumeClaimTemplate
 		if override, ok := crd.Spec.InstanceOverrides[instanceName(crd, i)]; ok {
@@ -64,7 +58,7 @@ func BuildPVCs(crd *cosmosv1.CosmosFullNode) []*corev1.PersistentVolumeClaim {
 		preserveMergeInto(pvc.Annotations, tpl.Metadata.Annotations)
 		kube.NormalizeMetadata(&pvc.ObjectMeta)
 
-		pvcs = append(pvcs, pvc)
+		pvcs = append(pvcs, diff.Adapt(pvc, i))
 	}
 	return pvcs
 }
@@ -91,20 +85,4 @@ func pvcResources(crd *cosmosv1.CosmosFullNode) corev1.ResourceRequirements {
 		}
 	}
 	return reqs
-}
-
-// Attempts to produce a deterministic hash based on the pvc template, so we can detect updates.
-// See podRevisionHash for more details.
-func pvcRevisionHash(crd *cosmosv1.CosmosFullNode) string {
-	h := fnv.New32()
-	mustWrite(h, mustMarshalJSON(crd.Spec.VolumeClaimTemplate))
-	mustWrite(h, mustMarshalJSON(crd.Status.SelfHealing.PVCAutoScale))
-
-	keys := maps.Keys(crd.Spec.InstanceOverrides)
-	sort.Strings(keys)
-	for _, k := range keys {
-		mustWrite(h, mustMarshalJSON(crd.Spec.InstanceOverrides[k].VolumeClaimTemplate))
-	}
-
-	return hex.EncodeToString(h.Sum(nil))
 }
