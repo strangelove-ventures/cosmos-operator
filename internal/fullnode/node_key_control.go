9d574f77660cb90a63121c228efd9334de6bfb1d
David Nix <hello@davidnix.io>
2023-04-19 13:55:37 -0600

    Node key control returns secrets

--- internal/fullnode/node_key_control.go
+++ internal/fullnode/node_key_control.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/samber/lo"
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
 	"github.com/strangelove-ventures/cosmos-operator/internal/diff"
 	"github.com/strangelove-ventures/cosmos-operator/internal/kube"
@@ -25,19 +26,20 @@ func NewNodeKeyControl(client Client) NodeKeyControl {
 }
 
 // Reconcile is the control loop for node keys. The secrets are never deleted.
-func (control NodeKeyControl) Reconcile(ctx context.Context, reporter kube.Reporter, crd *cosmosv1.CosmosFullNode) kube.ReconcileError {
+// The returned secrets are all the secrets that will be created given the CRD replicas.
+func (control NodeKeyControl) Reconcile(ctx context.Context, reporter kube.Reporter, crd *cosmosv1.CosmosFullNode) ([]*corev1.Secret, kube.ReconcileError) {
 	var secrets corev1.SecretList
 	if err := control.client.List(ctx, &secrets,
 		client.InNamespace(crd.Namespace),
 		client.MatchingFields{kube.ControllerOwnerField: crd.Name},
 	); err != nil {
-		return kube.TransientError(fmt.Errorf("list existing node key secrets: %w", err))
+		return nil, kube.TransientError(fmt.Errorf("list existing node key secrets: %w", err))
 	}
 
 	existing := ptrSlice(secrets.Items)
 	want, serr := BuildNodeKeySecrets(existing, crd)
 	if serr != nil {
-		return kube.UnrecoverableError(fmt.Errorf("build node key secrets: %w", serr))
+		return nil, kube.UnrecoverableError(fmt.Errorf("build node key secrets: %w", serr))
 	}
 
 	diffed := diff.New(existing, want)
@@ -45,19 +47,20 @@ func (control NodeKeyControl) Reconcile(ctx context.Context, reporter kube.Repor
 	for _, secret := range diffed.Creates() {
 		reporter.Info("Creating node key secret", "secret", secret.Name)
 		if err := ctrl.SetControllerReference(crd, secret, control.client.Scheme()); err != nil {
-			return kube.TransientError(fmt.Errorf("set controller reference on node key secret %q: %w", secret.Name, err))
+			return nil, kube.TransientError(fmt.Errorf("set controller reference on node key secret %q: %w", secret.Name, err))
 		}
 		if err := control.client.Create(ctx, secret); kube.IgnoreAlreadyExists(err) != nil {
-			return kube.TransientError(fmt.Errorf("create node key secret %q: %w", secret.Name, err))
+			return nil, kube.TransientError(fmt.Errorf("create node key secret %q: %w", secret.Name, err))
 		}
 	}
 
 	for _, secret := range diffed.Updates() {
 		reporter.Info("Updating node key secret", "secret", secret.Name)
 		if err := control.client.Update(ctx, secret); err != nil {
-			return kube.TransientError(fmt.Errorf("update node key secret %q: %w", secret.Name, err))
+			return nil, kube.TransientError(fmt.Errorf("update node key secret %q: %w", secret.Name, err))
 		}
 	}
 
-	return nil
+	builtSecrets := lo.Map(want, func(s diff.Resource[*corev1.Secret], _ int) *corev1.Secret { return s.Object() })
+	return builtSecrets, nil
 }
