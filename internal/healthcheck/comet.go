eda3ae20b5c3e920226c28ba5f589bdc802f462a
David Nix <hello@davidnix.io>
2023-05-23 10:17:51 -0600

    Add misspell linter ignored words

--- internal/healthcheck/comet.go
+++ internal/healthcheck/comet.go
@@ -1,8 +1,6 @@
 // Package healthcheck typically enables readiness or liveness probes within kubernetes.
 // IMPORTANT: If you update this behavior, be sure to update internal/fullnode/pod_builder.go with the new
 // cosmos operator image in the "healthcheck" container.
-//
-//nolint:misspell
 package healthcheck
 
 import (
