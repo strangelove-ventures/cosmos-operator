c7c3c8b567c4682a4e33bc7a0a5aa4e9411854fb
David Nix <hello@davidnix.io>
2023-05-24 16:42:09 -0600

    First pass at polling pods

--- internal/cosmos/cache_controller.go
+++ internal/cosmos/cache_controller.go
@@ -2,31 +2,69 @@ package cosmos
 
 import (
 	"context"
+	"fmt"
 	"sync"
+	"time"
 
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
+	"github.com/strangelove-ventures/cosmos-operator/internal/kube"
+	"golang.org/x/sync/errgroup"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type cache struct {
 	sync.RWMutex
-	m map[client.ObjectKey]StatusCollection
+	m map[client.ObjectKey]*cacheItem
+}
+
+type cacheItem struct {
+	coll   StatusCollection
+	cancel context.CancelFunc
+}
+
+func newCache() *cache {
+	return &cache{
+		m: make(map[client.ObjectKey]*cacheItem),
+	}
 }
 
 func (c *cache) Get(key client.ObjectKey) (StatusCollection, bool) {
 	c.RLock()
 	defer c.RUnlock()
 	v, ok := c.m[key]
-	return v, ok
+	if !ok {
+		return nil, false
+	}
+	return v.coll, ok
 }
 
-func (c *cache) Set(key client.ObjectKey, value StatusCollection) {
+func (c *cache) Init(key client.ObjectKey, cancel context.CancelFunc) {
 	c.Lock()
 	defer c.Unlock()
-	c.m[key] = value
+	c.m[key] = &cacheItem{
+		coll:   make(StatusCollection, 0),
+		cancel: cancel,
+	}
+}
+
+func (c *cache) Update(key client.ObjectKey, value StatusCollection) {
+	c.Lock()
+	defer c.Unlock()
+	c.m[key].coll = value
+}
+
+func (c *cache) Del(key client.ObjectKey) {
+	c.Lock()
+	defer c.Unlock()
+	if v, ok := c.m[key]; ok {
+		v.cancel()
+	}
+	delete(c.m, key)
 }
 
 type Collector interface {
@@ -34,22 +72,27 @@ type Collector interface {
 }
 
 // CacheController periodically polls pods for their CometBFT status and caches the result.
-// The cache is a controller so it can watch CosmosFullNode objects to invalidate and warm the cache.
+// The cache is a controller so it can watch CosmosFullNode objects to warm or invalidate the cache.
 type CacheController struct {
 	cache     *cache
 	client    client.Reader
 	collector Collector
+	eg        errgroup.Group
+	interval  time.Duration
+	recorder  record.EventRecorder
 }
 
-func NewCacheController(collector Collector, reader client.Reader) *CacheController {
+func NewCacheController(collector Collector, reader client.Reader, recorder record.EventRecorder) *CacheController {
 	return &CacheController{
-		cache:     new(cache),
+		cache:     newCache(),
 		client:    reader,
 		collector: collector,
+		interval:  5 * time.Second,
+		recorder:  recorder,
 	}
 }
 
-// SetupWithManager watches CosmosFullNode objects.
+// SetupWithManager watches CosmosFullNode objects and starts cache collecting.
 func (c *CacheController) SetupWithManager(_ context.Context, mgr ctrl.Manager) error {
 	// We do not index pods because we presume another controller is already doing so.
 	// If we repeat it here, the manager returns an error.
@@ -63,17 +106,62 @@ var finishResult reconcile.Result
 func (c *CacheController) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	crd := new(cosmosv1.CosmosFullNode)
 	if err := c.client.Get(ctx, req.NamespacedName, crd); err != nil {
-		// TODO: remove from cache
-		return finishResult, client.IgnoreNotFound(err)
+		if kube.IsNotFound(err) {
+			c.cache.Del(req.NamespacedName)
+		}
+		return finishResult, kube.IgnoreNotFound(err)
+	}
+
+	reporter := kube.NewEventReporter(log.FromContext(ctx), c.recorder, crd)
+
+	// If not already cached, start collecting status from pods.
+	if _, ok := c.cache.Get(req.NamespacedName); !ok {
+		cctx, cancel := context.WithCancel(ctx)
+		c.cache.Init(req.NamespacedName, cancel)
+		c.eg.Go(func() error {
+			defer cancel()
+			c.collectFromPods(cctx, reporter, req.NamespacedName)
+			return nil
+		})
 	}
 
 	// Fetch and add from cache.
-
 	return finishResult, nil
 }
 
-// Collect returns a StatusCollection for the given controller. For optimal performance, only returns cached results.
-// If the cache is stale, returns an empty StatusCollection.
-func (c *CacheController) Collect(ctx context.Context, controller client.ObjectKey) StatusCollection {
-	return nil
+// Collect returns a StatusCollection for the given controller. Only returns cached results or an empty
+// collection if nothing is cached.
+func (c *CacheController) Collect(controller client.ObjectKey) StatusCollection {
+	v, _ := c.cache.Get(controller)
+	return v
+}
+
+func (c *CacheController) collectFromPods(ctx context.Context, reporter kube.Reporter, controller client.ObjectKey) {
+	defer c.cache.Del(controller)
+
+	collect := func() {
+		var pods corev1.PodList
+		if err := c.client.List(ctx, &pods,
+			client.InNamespace(controller.Namespace),
+			client.MatchingFields{kube.ControllerOwnerField: controller.Name},
+		); err != nil {
+			err = fmt.Errorf("%s: %w", controller, err)
+			reporter.Error(err, "Failed to list pods")
+			reporter.RecordError("ListPods", err)
+			return
+		}
+		c.cache.Update(controller, c.collector.Collect(ctx, pods.Items))
+	}
+
+	collect() // Collect once immediately.
+	tick := time.NewTicker(c.interval)
+	defer tick.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-tick.C:
+			collect()
+		}
+	}
 }
--- internal/cosmos/cache_controller_test.go
+++ internal/cosmos/cache_controller_test.go
@@ -2,24 +2,111 @@ package cosmos
 
 import (
 	"context"
+	"sync/atomic"
 	"testing"
 
+	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type mockCollector struct {
+	Called         int64
+	WaitCh         chan struct{}
+	GotPods        []corev1.Pod
+	StubCollection StatusCollection
 }
 
-func (m mockCollector) Collect(ctx context.Context, pods []corev1.Pod) StatusCollection {
+func (m *mockCollector) Collect(ctx context.Context, pods []corev1.Pod) StatusCollection {
+	if m.WaitCh == nil {
+		panic("nil wait channel")
+	}
+	defer func() { m.WaitCh <- struct{}{} }()
+	atomic.AddInt64(&m.Called, 1)
+	if ctx == nil {
+		panic("nil context")
+	}
+	m.GotPods = pods
+	return m.StubCollection
+}
+
+type mockReader struct {
+	ListPods []corev1.Pod
+	ListOpts []client.ListOption
+}
+
+func (m *mockReader) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
+	if ctx == nil {
+		panic("nil context")
+	}
+	var crd cosmosv1.CosmosFullNode
+	crd.Name = key.Name
+	crd.Namespace = key.Namespace
+	*obj.(*cosmosv1.CosmosFullNode) = crd
 	return nil
 }
 
-func TestStatusCache_Reconcile(t *testing.T) {
-	t.Run("create, update, or patch", func(t *testing.T) {
+func (m *mockReader) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
+	if ctx == nil {
+		panic("nil context")
+	}
+	m.ListOpts = opts
+	list.(*corev1.PodList).Items = m.ListPods
+	return nil
+}
 
+func TestCacheController_Reconcile(t *testing.T) {
+	mockRecorder := struct {
+		record.EventRecorder
+	}{}
+
+	ctx := context.Background()
+	const (
+		namespace = "strangelove"
+		name      = "nolus"
+	)
+
+	t.Run("crd created or updated", func(t *testing.T) {
+		pods := make([]corev1.Pod, 2)
+		var reader mockReader
+		reader.ListPods = pods
+
+		var collector mockCollector
+		collector.WaitCh = make(chan struct{}, 1)
+		collector.StubCollection = make(StatusCollection, 3)
+
+		controller := NewCacheController(&collector, &reader, mockRecorder)
+
+		var req reconcile.Request
+		req.Name = name
+		req.Namespace = namespace
+		res, err := controller.Reconcile(ctx, req)
+
+		require.Equal(t, reconcile.Result{}, res)
+		require.NoError(t, err)
+
+		<-collector.WaitCh
+		require.Equal(t, pods, collector.GotPods)
+
+		opts := reader.ListOpts
+		require.Len(t, opts, 2)
+		var listOpt client.ListOptions
+		for _, opt := range opts {
+			opt.ApplyToList(&listOpt)
+		}
+		require.Equal(t, namespace, listOpt.Namespace)
+		require.Zero(t, listOpt.Limit)
+		require.Equal(t, ".metadata.controller=nolus", listOpt.FieldSelector.String())
+
+		gotCollection := controller.Collect(client.ObjectKey{Name: name, Namespace: namespace})
+		require.Equal(t, collector.StubCollection, gotCollection)
+		require.Equal(t, int64(1), collector.Called) // Uses the cache
 	})
 
-	t.Run("delete", func(t *testing.T) {
-
+	t.Run("crd deleted", func(t *testing.T) {
+		t.Fail()
 	})
 }
