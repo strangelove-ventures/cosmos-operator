c2389cc31fa8d5e9771eff57b75619488b8ffa5b
David Nix <hello@davidnix.io>
2023-05-31 12:16:58 -0600

    tmp: Debug lines because I'm lazy

--- controllers/selfhealing_controller.go
+++ controllers/selfhealing_controller.go
@@ -19,6 +19,7 @@ package controllers
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -65,6 +66,7 @@ func NewSelfHealing(
 // uses the status object to reconcile its state.
 func (r *SelfHealingReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx).WithName(cosmosv1.SelfHealingController)
+	fmt.Println("!!!!!!!Reconciling SelfHealing!!!!!!!")
 	logger.V(1).Info("Entering reconcile loop", "request", req.NamespacedName)
 
 	crd := new(cosmosv1.CosmosFullNode)
