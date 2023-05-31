edd4192fe19a8f7f23729114d78ff2e29a64f48f
David Nix <hello@davidnix.io>
2023-05-31 10:45:55 -0600

    Create consensus status from pods

--- internal/fullnode/status.go
+++ internal/fullnode/status.go
@@ -1,7 +1,13 @@
 package fullnode
 
 import (
+	"context"
+
+	"github.com/samber/lo"
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
+	"github.com/strangelove-ventures/cosmos-operator/internal/cosmos"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // ResetStatus is used at the beginning of the reconcile loop.
@@ -10,4 +16,34 @@ func ResetStatus(crd *cosmosv1.CosmosFullNode) {
 	crd.Status.ObservedGeneration = crd.Generation
 	crd.Status.Phase = cosmosv1.FullNodePhaseProgressing
 	crd.Status.StatusMessage = nil
+}
+
+type StatusCollector interface {
+	Collect(ctx context.Context, controller client.ObjectKey) cosmos.StatusCollection
+}
+
+func ConsensusStatus(
+	ctx context.Context,
+	crd *cosmosv1.CosmosFullNode,
+	collector StatusCollector,
+) cosmosv1.ConsensusStatus {
+	var status cosmosv1.ConsensusStatus
+
+	coll := collector.Collect(ctx, client.ObjectKeyFromObject(crd))
+
+	status.Pods = lo.Map(coll, func(item cosmos.StatusItem, _ int) cosmosv1.ConsensusPodStatus {
+		var stat cosmosv1.ConsensusPodStatus
+		stat.Pod = item.GetPod().Name
+		stat.Timestamp = metav1.NewTime(item.Timestamp())
+		comet, err := item.GetStatus()
+		if err != nil {
+			stat.Error = ptr(err.Error())
+			return stat
+		}
+		stat.Height = ptr(comet.LatestBlockHeight())
+		stat.InSync = ptr(!comet.Result.SyncInfo.CatchingUp)
+		return stat
+	})
+
+	return status
 }
--- internal/fullnode/status_test.go
+++ internal/fullnode/status_test.go
@@ -1,24 +1,98 @@
 package fullnode
 
 import (
+	"context"
+	"errors"
 	"testing"
+	"time"
 
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
+	"github.com/strangelove-ventures/cosmos-operator/internal/cosmos"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestResetStatus(t *testing.T) {
 	t.Parallel()
 
-	t.Run("basic happy path", func(t *testing.T) {
-		var crd cosmosv1.CosmosFullNode
-		crd.Generation = 123
-		crd.Status.StatusMessage = ptr("should not see me")
-		crd.Status.Phase = "should not see me"
-		ResetStatus(&crd)
+	var crd cosmosv1.CosmosFullNode
+	crd.Generation = 123
+	crd.Status.StatusMessage = ptr("should not see me")
+	crd.Status.Phase = "should not see me"
+	ResetStatus(&crd)
 
-		require.EqualValues(t, 123, crd.Status.ObservedGeneration)
-		require.Nil(t, crd.Status.StatusMessage)
-		require.Equal(t, cosmosv1.FullNodePhaseProgressing, crd.Status.Phase)
-	})
+	require.EqualValues(t, 123, crd.Status.ObservedGeneration)
+	require.Nil(t, crd.Status.StatusMessage)
+	require.Equal(t, cosmosv1.FullNodePhaseProgressing, crd.Status.Phase)
+}
+
+type mockStatusCollector struct {
+	CollectFn func(ctx context.Context, controller client.ObjectKey) cosmos.StatusCollection
+}
+
+func (m mockStatusCollector) Collect(ctx context.Context, controller client.ObjectKey) cosmos.StatusCollection {
+	return m.CollectFn(ctx, controller)
+}
+
+func TestConsensusStatus(t *testing.T) {
+	t.Parallel()
+
+	const (
+		name      = "agoric"
+		namespace = "default"
+	)
+
+	var crd cosmosv1.CosmosFullNode
+	crd.Name = name
+	crd.Namespace = namespace
+
+	ts := time.Now()
+
+	var collector mockStatusCollector
+	collector.CollectFn = func(ctx context.Context, controller client.ObjectKey) cosmos.StatusCollection {
+		require.NotNil(t, ctx)
+		require.Equal(t, name, controller.Name)
+		require.Equal(t, namespace, controller.Namespace)
+
+		var notInSync cosmos.CometStatus
+		notInSync.Result.SyncInfo.CatchingUp = true
+		notInSync.Result.SyncInfo.LatestBlockHeight = "9999"
+
+		var inSync cosmos.CometStatus
+		inSync.Result.SyncInfo.LatestBlockHeight = "10000"
+
+		return cosmos.StatusCollection{
+			// Purposefully out of order to test sorting.
+			{Pod: &corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod-0"}}, Status: notInSync, Ts: ts},
+			{Pod: &corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod-1"}}, Status: inSync, Ts: ts},
+			{Pod: &corev1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "pod-2"}}, Err: errors.New("some error"), Ts: ts},
+		}
+	}
+
+	wantTs := metav1.NewTime(ts)
+	want := cosmosv1.ConsensusStatus{
+		Pods: []cosmosv1.ConsensusPodStatus{
+			{
+				Pod:       "pod-0",
+				Timestamp: wantTs,
+				Height:    ptr(uint64(9999)),
+				InSync:    ptr(false),
+			},
+			{Pod: "pod-1",
+				Timestamp: wantTs,
+				Height:    ptr(uint64(10000)),
+				InSync:    ptr(true),
+			},
+			{
+				Pod:       "pod-2",
+				Timestamp: wantTs,
+				Error:     ptr("some error"),
+			},
+		},
+	}
+
+	status := ConsensusStatus(context.Background(), &crd, collector)
+	require.Equal(t, want, status)
 }
