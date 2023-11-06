9b9c2604ed68646aeba643ed88b53336ba60ab89
Andrew Gouin <andrew@gouin.io>
2023-11-06 11:47:20 -0700

    Remove dependency on cometbft

--- internal/fullnode/node_key_builder.go
+++ internal/fullnode/node_key_builder.go
@@ -1,11 +1,13 @@
 package fullnode
 
 import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 
-	"github.com/cometbft/cometbft/crypto/ed25519"
-	cmtjson "github.com/cometbft/cometbft/libs/json"
-	"github.com/cometbft/cometbft/p2p"
 	cosmosv1 "github.com/strangelove-ventures/cosmos-operator/api/v1"
 	"github.com/strangelove-ventures/cosmos-operator/internal/diff"
 	"github.com/strangelove-ventures/cosmos-operator/internal/kube"
@@ -41,19 +43,46 @@ func BuildNodeKeySecrets(existing []*corev1.Secret, crd *cosmosv1.CosmosFullNode
 
 		// Create node key if it doesn't exist
 		if secret.Data[nodeKeyFile] == nil {
-			nodeKey := p2p.NodeKey{PrivKey: ed25519.GenPrivKey()}
-			b, err := cmtjson.Marshal(nodeKey)
+			nk, err := randNodeKey()
 			if err != nil {
 				return nil, err
 			}
 			secret.Data = map[string][]byte{
-				nodeKeyFile: b,
+				nodeKeyFile: nk,
 			}
 		}
 
 		secrets[i] = diff.Adapt(&secret, i)
 	}
 	return secrets, nil
+}
+
+type NodeKey struct {
+	PrivKey NodeKeyPrivKey `json:"priv_key"`
+}
+
+type NodeKeyPrivKey struct {
+	Type  string             `json:"type"`
+	Value ed25519.PrivateKey `json:"value"`
+}
+
+func (nk NodeKey) ID() string {
+	pub := nk.PrivKey.Value.Public()
+	hash := sha256.Sum256(pub.(ed25519.PublicKey))
+	return hex.EncodeToString(hash[:20])
+}
+
+func randNodeKey() ([]byte, error) {
+	_, pk, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate ed25519 node key: %w", err)
+	}
+	return json.Marshal(NodeKey{
+		PrivKey: NodeKeyPrivKey{
+			Type:  "tendermint/PrivKeyEd25519",
+			Value: pk,
+		},
+	})
 }
 
 func nodeKeySecretName(crd *cosmosv1.CosmosFullNode, ordinal int32) string {
