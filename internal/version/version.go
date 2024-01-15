89202e9baca2174bb403a2765f66843e3e22226f
jinu <jinwoo794533@gmail.com>
2024-01-15 16:17:21 +0900

    rollback test

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 // version is the version of the build.
 // Set via ldflags at build time.
 // Used for docker image.
-// See Dockerfile, Makefile, and .github/.workflows/release.yaml.
+// See Dockerfile, Makefile, and .github/workflows/release.yaml.
 var version = ""
 
 // DockerTag returns the version of the build or "latest" if unknown.
