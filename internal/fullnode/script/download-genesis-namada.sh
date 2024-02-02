f1d09ec28b21766b4dcbfd5d589c3b5bd2690f60
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-02 17:45:59 +0900

    check already downloaded

--- internal/fullnode/script/download-genesis-namada.sh
+++ internal/fullnode/script/download-genesis-namada.sh
@@ -1,6 +1,10 @@
 set -eu
 
-
 CHAIN_ID=$0
 NAMADA_NETWORK_CONFIGS_SERVER="https://github.com/anoma/namada-shielded-expedition/releases/download/$CHAIN_ID"
+
+if [ ! -d "$CHAIN_ID" ]; then
+        echo "Already downloaded"
+        exit 0
+fi
 namada --base-dir $CHAIN_HOME client utils join-network --chain-id $CHAIN_ID
