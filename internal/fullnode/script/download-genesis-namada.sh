f23d400a589b0e867df71e0357e94ade92db2531
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-02 17:27:00 +0900

    fix

--- internal/fullnode/script/download-genesis-namada.sh
+++ internal/fullnode/script/download-genesis-namada.sh
@@ -1,7 +1,7 @@
 set -eu
 
 
-CHAIN_ID=$1
+CHAIN_ID=$0
 NAMADA_NETWORK_CONFIGS_SERVER="https://github.com/anoma/namada-shielded-expedition/releases/download/$CHAIN_ID"
-NAMADA_NETWORK_CONFIGS_SERVER=$2
-namada --base-dir /data/namada client utils join-network --chain-id $CHAIN_ID
+NAMADA_NETWORK_CONFIGS_SERVER=$1
+namada --base-dir $CHAIN_HOME client utils join-network --chain-id $CHAIN_ID
