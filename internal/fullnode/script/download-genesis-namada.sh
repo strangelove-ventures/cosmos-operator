cbe8c65fae41f56267d37790d17709fe044c8d4e
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-04 01:16:25 +0900

    fix script

--- internal/fullnode/script/download-genesis-namada.sh
+++ internal/fullnode/script/download-genesis-namada.sh
@@ -1,10 +1,15 @@
 set -eu
 
+echo $USER
+
 CHAIN_ID=$0
 NAMADA_NETWORK_CONFIGS_SERVER="https://github.com/anoma/namada-shielded-expedition/releases/download/$CHAIN_ID"
 
 if [ ! -d "$CHAIN_ID" ]; then
-        echo "Already downloaded"
-        exit 0
+    echo "Directory $CHAIN_ID does not exist. Downloading..."
+    namada --base-dir $CHAIN_HOME client utils join-network --chain-id $CHAIN_ID
+    mkdir -p "$CHAIN_ID"
+    echo "$CHAIN_ID downloaded successfully."
+else
+    echo "Directory $CHAIN_ID already exists."
 fi
-namada --base-dir $CHAIN_HOME client utils join-network --chain-id $CHAIN_ID
