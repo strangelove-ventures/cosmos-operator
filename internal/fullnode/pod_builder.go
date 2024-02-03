03520543d27efbe8b3d5e53455d55eebf6be389c
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-04 01:52:06 +0900

    edit

--- internal/fullnode/pod_builder.go
+++ internal/fullnode/pod_builder.go
@@ -507,7 +507,7 @@ func startCmdAndArgs(crd *cosmosv1.CosmosFullNode) (string, []string) {
 	if crd.Spec.ChainSpec.ChainType == chainTypeCosmovisor {
 		binary = "/bin/sh"
 	} else if crd.Spec.ChainSpec.ChainType == chainTypeNamada {
-		binary = "/bin/sh"
+		binary = "sh"
 	}
 
 	if v := crd.Spec.ChainSpec.PrivvalSleepSeconds; v != nil {
@@ -531,7 +531,7 @@ func startCommandArgs(crd *cosmosv1.CosmosFullNode) []string {
 		originArgs := args
 		args = []string{"-c", "/bin/cosmovisor init /bin/" + cfg.Binary + "; " + "/bin/cosmovisor run " + strings.Join(originArgs, " ")}
 	} else if crd.Spec.ChainSpec.ChainType == chainTypeNamada {
-		args = []string{"-c", "/bin/namada --base-dir " + ChainHomeDir(crd) + " --chain-id " + crd.Spec.ChainSpec.ChainID + " node ledger run"}
+		args = []string{"-c", "namada --base-dir " + ChainHomeDir(crd) + " --chain-id " + crd.Spec.ChainSpec.ChainID + " node ledger run"}
 		return args
 	}
 
