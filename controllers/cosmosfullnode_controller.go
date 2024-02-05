9f01a7b16bf4f15fce233551581f320d1ac6dbf3
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-05 13:23:28 +0900

    add PeerCollect debugging

--- controllers/cosmosfullnode_controller.go
+++ controllers/cosmosfullnode_controller.go
@@ -146,6 +146,7 @@ func (r *CosmosFullNodeReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 
 	// Find peer information that's used downstream.
 	peers, perr := r.peerCollector.Collect(ctx, crd)
+	reporter.Debug(strings.Join(peers.AllExternal(), ", "))
 	if perr != nil {
 		peers = peers.Default()
 		errs.Append(perr)
