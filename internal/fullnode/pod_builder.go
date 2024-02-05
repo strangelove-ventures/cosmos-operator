3510807586a8749d43e8e28c60d496fbf176c1c6
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-05 12:36:36 +0900

    add ls before/after namada run

--- internal/fullnode/pod_builder.go
+++ internal/fullnode/pod_builder.go
@@ -554,7 +554,7 @@ func startCommandArgs(crd *cosmosv1.CosmosFullNode) []string {
 		originArgs := args
 		args = []string{"-c", "/bin/cosmovisor init /bin/" + cfg.Binary + "; " + "/bin/cosmovisor run " + strings.Join(originArgs, " ")}
 	} else if crd.Spec.ChainSpec.ChainType == chainTypeNamada {
-		args = []string{"-c", "ls -al ; namada --base-dir " + ChainHomeDir(crd) + " --chain-id " + crd.Spec.ChainSpec.ChainID + " node ledger run; trap : TERM INT; sleep infinity & wait"}
+		args = []string{"-c", "ls -al namada/" + crd.Spec.ChainSpec.ChainID + "; namada --base-dir " + ChainHomeDir(crd) + " --chain-id " + crd.Spec.ChainSpec.ChainID + " node ledger run;ls -al namada/" + crd.Spec.ChainSpec.ChainID + " trap : TERM INT; sleep infinity & wait"}
 		return args
 	}
 
