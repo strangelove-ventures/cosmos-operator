86725d941b338100b6211f8431a52746d289434a
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-06 02:06:06 +0900

    test

--- internal/fullnode/pod_builder.go
+++ internal/fullnode/pod_builder.go
@@ -345,7 +345,7 @@ func getCleanInitContainer(env []corev1.EnvVar, tpl cosmosv1.PodSpec) corev1.Con
 		Name:            "clean-init",
 		Image:           infraToolImage,
 		Command:         []string{"sh"},
-		Args:            []string{"-c", `if [ -d $HOME/.tmp/ ]; then rm -rf "$HOME/.tmp/*"; fi`},
+		Args:            []string{"-c", `ls -al $HOME; if [ -d $HOME/.tmp/ ]; then rm -rf "$HOME/.tmp/*"; fi`},
 		Env:             env,
 		ImagePullPolicy: tpl.ImagePullPolicy,
 		WorkingDir:      workDir,
@@ -360,17 +360,14 @@ func getCosmosChainInitContainer(env []corev1.EnvVar, tpl cosmosv1.PodSpec, init
 		Args: []string{"-c",
 			fmt.Sprintf(`
 set -eu
-ls -al $HOME
 if [ ! -d "$COMETBFT_HOME/data" ]; then
 	echo "Initializing chain..."
 	%s --home "$COMETBFT_HOME"
 else
 	echo "Skipping chain init; already initialized."
 fi
-ls -al $HOME
 echo "Initializing into tmp dir for downstream processing..."
 %s --home "$HOME/.tmp"
-ls -al $HOME
 `, initCmd, initCmd),
 		},
 		Env:             env,
