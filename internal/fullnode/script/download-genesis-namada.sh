025bb46098a85489f4dd764672a68180ed34ba2d
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-06 02:31:31 +0900

    change checking target dir -> file

--- internal/fullnode/script/download-genesis-namada.sh
+++ internal/fullnode/script/download-genesis-namada.sh
@@ -1,6 +1,6 @@
 NAMADA_NETWORK_CONFIGS_SERVER="https://github.com/anoma/namada-shielded-expedition/releases/download/$CHAIN_ID"
 
-if [ ! -d $CHAIN_HOME/$CHAIN_ID ]; then
+if [ ! -f $CHAIN_HOME/$CHAIN_ID/validity-predicates.toml ]; then
     echo "Directory $CHAIN_ID does not exist. Downloading..."
     namada --base-dir $CHAIN_HOME client utils join-network --chain-id "$CHAIN_ID"
     mkdir -p $CHAIN_HOME/$CHAIN_ID/cometbft/
