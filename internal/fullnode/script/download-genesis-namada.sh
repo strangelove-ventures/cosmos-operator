f7b781ed65c38af39c57589d2659a55052dd90d8
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-13 11:11:28 +0900

    Fix mistake: mkdir -p ~/.tmp/config/config.toml

--- internal/fullnode/script/download-genesis-namada.sh
+++ internal/fullnode/script/download-genesis-namada.sh
@@ -3,7 +3,7 @@ NAMADA_NETWORK_CONFIGS_SERVER="https://github.com/anoma/namada-shielded-expediti
 if [ ! -f $CHAIN_HOME/$CHAIN_ID/validity-predicates.toml ]; then
     echo "Directory $CHAIN_ID does not exist. Downloading..."
     namada --base-dir $CHAIN_HOME client utils join-network --chain-id "$CHAIN_ID"
-    mkdir -p /home/operator/.tmp/config/config.toml
+    mkdir -p /home/operator/.tmp/config
     cp $CHAIN_HOME/$CHAIN_ID/config.toml /home/operator/.tmp/config/
     echo "$CHAIN_ID downloaded successfully."
 else
