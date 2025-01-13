ff133e55eb24466b07d96e5799229cfe7552922d
vimystic <122659254+vimystic@users.noreply.github.com>
2025-01-13 02:43:14 -0700

    add to newManager

--- main.go
+++ main.go
@@ -146,6 +146,7 @@ func startManager(cmd *cobra.Command, args []string) error {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "16e1bc09.strange.love",
+		Namespace:              namespaceRegex,
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
